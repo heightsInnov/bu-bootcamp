235c66f88e290289d4f25d62d926e80409c5465f
ayotola <ayotola.jinadu@hitechnologies.org>
2026-08-30 11:54:50 -0300

    Add Module 1 activity: Hello World and Greeting programs

---
+++ Module1/Arithmetic.java
@@ -0,0 +1,9 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        double result = a/b;
+        double correct = (double) a/b;
+        System.out.println(result);
+    }
+}
